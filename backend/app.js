8bfdfb1f5a87a7ef8198093bc0c0803013dfc981
andxbes <andxbes@gmail.com>
2024-12-03 20:53:47 +0200

    420

--- backend/app.js
+++ backend/app.js
@@ -76,6 +76,10 @@ mongoose
     MONGODB_URI
   )
   .then(result => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require('./socket').init(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
   })
   .catch(err => console.log(err));
